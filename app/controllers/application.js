f92951acb8962f55b1fbd94d099c858e20938fe2
Muhamad Barhan <muhamadbarhan02@gmail.com>
2025-05-02 09:39:12 +0530

    Latest commit

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,6 +1,7 @@
 import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object'
+import { task, timeout } from "ember-concurrency"
 
 export default class ApplicationController extends Controller {
 
@@ -9,6 +10,7 @@ export default class ApplicationController extends Controller {
         this.loadAmount();
         this.loadSubscription();
         this.loadHistory();
+        this.checkSubscription.perform();
     }
 
     @tracked amount = 0;
@@ -19,6 +21,8 @@ export default class ApplicationController extends Controller {
     @tracked isFilterOpen = false;
 
     @tracked subscriptions = [];
+    @tracked history = [];
+    @tracked filteredHistory = [];
 
     @tracked name = '';
     @tracked subplan = '';
@@ -27,13 +31,11 @@ export default class ApplicationController extends Controller {
     @tracked category = '';
     @tracked paymethod = '';
 
+    months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
     plans = ['Standard', 'Pro', 'Pro+'];
-    billings = ['Weekly', 'Monthly', 'Yearly'];
+    billings = ['Weekly', 'Monthly', 'Yearly', 'Minute', 'Seconds'];
     categories = ['Entertainment', 'Music', 'Amazon Prime', 'Jio Star'];
     paymentMethod = ['UPI', 'Debit Card', 'Net Banking', 'Wallet'];
-
-    @tracked history = [];
-
 
     @action toggleModal() {
         this.isModelOpen = !this.isModelOpen;
@@ -49,20 +51,32 @@ export default class ApplicationController extends Controller {
     }
 
     @action addToWallet() {
-        // let credit = {
-        //     name: this.subscriptions[index].name,
-        //     subamount: this.subscriptions[index].subamount,
-        //     transaction:"Refund"
-        // }
         const value = document.getElementById('amount-value').value;
-        this.amount = parseInt(this.amount) + parseInt(value);
-        localStorage.setItem("amount", this.amount);
-        this.toggleModal();
-    }
+        if (value == '') {
+            alert("Enter the amount");
+            return;
+        }
+        let credit = {
+            name: "Wallet",
+            subamount: value,
+            transaction: "Credit",
+            subdate: {
+                date: new Date().getDate(),
+                day: new Date().getDay(),
+                hour: new Date().getHours(),
+                minute: new Date().getMinutes() < 10 ? "0" + new Date().getMinutes() : new Date().getMinutes(),
+                seconds: new Date().getSeconds(),
+                month: this.months[new Date().getMonth()],
+                year: new Date().getFullYear(),
+            }
+        }
 
-    @action loadAmount() {
-        const resAmount = localStorage.getItem("amount") || 0;
-        this.amount = resAmount;
+        this.amount = parseInt(this.amount) + parseInt(value);
+        this.history = this.history ? [...this.history, credit] : [credit];
+        localStorage.setItem("history", JSON.stringify(this.history));
+        localStorage.setItem("amount", this.amount);
+        this.loadHistory();
+        this.toggleModal();
     }
 
     @action updateName(event) {
@@ -89,6 +103,30 @@ export default class ApplicationController extends Controller {
         this.paymethod = pay;
     }
 
+    @action validitySetter(newDate) {
+        return {
+            dateObject: newDate,
+            date: newDate.getDate(),
+            day: newDate.getDay(),
+            hour: newDate.getHours(),
+            minute: newDate.getMinutes() < 10 ? "0" + newDate.getMinutes() : newDate.getMinutes(),
+            seconds: newDate.getSeconds(),
+            month: this.months[newDate.getMonth()],
+            year: newDate.getFullYear(),
+        }
+    }
+
+    @action addToLocal(subscription) {
+        this.subscriptions = this.subscriptions ? [...this.subscriptions, subscription] : [subscription];
+        localStorage.setItem("subscriptions", JSON.stringify(this.subscriptions));
+        this.loadSubscription();
+        this.history = this.history ? [...this.history, subscription] : [subscription];
+        localStorage.setItem("history", JSON.stringify(this.history));
+        this.loadHistory();
+        this.toggleSubModal();
+        this.resetInputs();
+    }
+
     @action addSubscription() {
         let subscription = {
             name: this.name,
@@ -104,27 +142,68 @@ export default class ApplicationController extends Controller {
                 if (parseInt(this.subamount) <= parseInt(this.amount)) {
                     this.amount = this.amount - this.subamount;
                     localStorage.setItem("amount", this.amount);
-                    this.subscriptions = this.subscriptions ? [...this.subscriptions, subscription] : [subscription];
-                    localStorage.setItem("subscriptions", JSON.stringify(this.subscriptions));
-                    this.loadSubscription();
-                    this.toggleSubModal();
-                    this.resetInputs();
                     subscription.transaction = "Debit";
-                    this.history = this.history ? [...this.history, subscription] : [subscription];
-                    localStorage.setItem("history", JSON.stringify(this.history));
-                    this.loadHistory();
+                    subscription.subdate = {
+                        date: new Date().getDate(),
+                        day: new Date().getDay(),
+                        hour: new Date().getHours(),
+                        minute: new Date().getMinutes() < 10 ? "0" + new Date().getMinutes() : new Date().getMinutes(),
+                        seconds: new Date().getSeconds(),
+                        month: this.months[new Date().getMonth()],
+                        year: new Date().getFullYear(),
+                    };
+                    if (subscription.billcycle == "Monthly") {
+                        var newDate = new Date(new Date().setDate(new Date().getDate() + 28));
+                        subscription.validity = this.validitySetter(newDate);
+
+                    } else if (subscription.billcycle == "Weekly") {
+                        var newDate = new Date(new Date().setDate(new Date().getDate() + 7));
+                        subscription.validity = this.validitySetter(newDate);
+
+                    } else if (subscription.billcycle == "Yearly") {
+                        var newDate = new Date(new Date().setDate(new Date().getDate() + 365));
+                        subscription.validity = this.validitySetter(newDate);
+
+                    } else if (subscription.billcycle == "Minute") {
+                        var newDate = new Date(new Date().setMinutes(new Date().getMinutes() + 1));
+                        subscription.validity = this.validitySetter(newDate);
+
+                    } else if (subscription.billcycle == "Seconds") {
+                        var newDate = new Date(new Date().setSeconds(new Date().getSeconds() + 5));
+                        console.log(newDate);
+                        subscription.validity = this.validitySetter(newDate);
+                    }
+
+                    this.addToLocal(subscription);
                 } else {
                     alert("Insufficient Wallet Balance");
                 }
             } else {
-                this.subscriptions = this.subscriptions ? [...this.subscriptions, subscription] : [subscription];;
-                localStorage.setItem("subscriptions", JSON.stringify(this.subscriptions));
-                this.loadSubscription();
-                this.toggleSubModal();
-                this.resetInputs();
+                this.addToLocal(subscription);
             }
 
         } else {
+            if (subscription.billcycle == "Monthly") {
+                var newDate = new Date(new Date().setDate(new Date().getDate() + 28));
+                subscription.validity = this.validitySetter(newDate);
+
+            } else if (subscription.billcycle == "Weekly") {
+                var newDate = new Date(new Date().setDate(new Date().getDate() + 7));
+                subscription.validity = this.validitySetter(newDate);
+
+            } else if (subscription.billcycle == "Yearly") {
+                var newDate = new Date(new Date().setDate(new Date().getDate() + 365));
+                subscription.validity = this.validitySetter(newDate);
+
+            } else if (subscription.billcycle == "Minute") {
+                var newDate = new Date(new Date().setMinutes(new Date().getMinutes() + 1));
+                subscription.validity = this.validitySetter(newDate);
+
+            } else if (subscription.billcycle == "Seconds") {
+                var newDate = new Date(new Date().setSeconds(new Date().getSeconds() + 20));
+                console.log(newDate);
+                subscription.validity = this.validitySetter(newDate);
+            }
             this.subscriptions[this.editIndex] = subscription;
             this.isEditing = !this.isEditing;
             localStorage.setItem("subscriptions", JSON.stringify(this.subscriptions))
@@ -155,7 +234,6 @@ export default class ApplicationController extends Controller {
     }
 
     @action cancelSubscription(index) {
-
         let cancelled = {
             name: this.subscriptions[index].name,
             subplan: this.subscriptions[index].subplan,
@@ -163,12 +241,21 @@ export default class ApplicationController extends Controller {
             subamount: this.subscriptions[index].subamount,
             category: this.subscriptions[index].category,
             paymethod: this.subscriptions[index].paymethod,
-            transaction:"Refund"
+            transaction: "Refund"
         }
 
         if (this.subscriptions[index].paymethod == "Wallet") {
             this.amount = parseInt(this.amount) + parseInt(this.subscriptions[index].subamount);
             localStorage.setItem("amount", this.amount);
+            cancelled.subdate = {
+                date: new Date().getDate(),
+                day: new Date().getDay(),
+                hour: new Date().getHours(),
+                minute: new Date().getMinutes() < 10 ? "0" + new Date().getMinutes() : new Date().getMinutes(),
+                seconds: new Date().getSeconds(),
+                month: this.months[new Date().getMonth()],
+                year: new Date().getFullYear(),
+            };
             this.history = this.history ? [...this.history, cancelled] : [cancelled];
             localStorage.setItem("history", JSON.stringify(this.history));
             this.loadHistory();
@@ -180,6 +267,30 @@ export default class ApplicationController extends Controller {
         this.loadSubscription();
     }
 
+    @action applyFilter() {
+        let checkboxes = Array.from(document.querySelectorAll('input[name="filter-check"]:checked'));
+        let checked = checkboxes.map(cb => cb.value)
+        console.log(checked)
+        if (checked == '') {
+            alert("Select a filter");
+            return;
+        }
+        this.filteredHistory = this.history.filter(h => {
+            checked.includes(h.transaction.toLowerCase())
+            console.log(checked.includes(h.transaction.toLowerCase()))
+            // console.log(h.transaction.toLowerCase())
+        }
+            );
+        this.toggleFilter();
+    }
+
+    @action clearFilter() {
+        let cbs = document.querySelectorAll('input[name="filter-check"]:checked')
+        cbs.forEach(cb => cb.checked = false);
+        this.filteredHistory = this.history;
+        this.toggleFilter();
+    }
+
     @action resetInputs() {
         this.name = '';
         this.subplan = '';
@@ -189,15 +300,68 @@ export default class ApplicationController extends Controller {
         this.paymethod = '';
     }
 
+    @action loadAmount() {
+        const resAmount = localStorage.getItem("amount") || 0;
+        this.amount = resAmount;
+    }
+
     @action loadSubscription() {
         const resSubscription = JSON.parse(localStorage.getItem("subscriptions"));
         this.subscriptions = resSubscription;
-        console.log(this.subscriptions);
     }
 
     @action loadHistory() {
         const resHistory = JSON.parse(localStorage.getItem("history"));
         this.history = resHistory;
-        console.log(this.history);
+        this.filteredHistory = this.history;
+    }
+
+
+    @task *checkSubscription() {
+        while (true) {
+            console.log("Running")
+
+            if (!this.subscriptions == '') {
+                this.subscriptions.forEach((subs, index) => {
+
+                    if (subs.billcycle == "Seconds" && new Date(subs.validity.dateObject) < new Date()) {
+                        // console.log(new Date(subs.validity.dateObject) < new Date())
+                        if (this.amount <= 0 || parseInt(this.amount) < parseInt(subs.subamount)) {
+                            subs.notify = "Subscription ended-Wallet amount is insufficent"
+                            this.subscriptions[index] = subs;
+                            localStorage.setItem("subscriptions", JSON.stringify(this.subscriptions));
+                            this.loadSubscription();
+                        } else {
+                            subs.notify = ""
+                            var newDate = new Date(new Date().setSeconds(new Date().getSeconds() + 20));
+                            subs.subdate = {
+                                date: new Date().getDate(),
+                                day: new Date().getDay(),
+                                hour: new Date().getHours(),
+                                minute: new Date().getMinutes() < 10 ? "0" + new Date().getMinutes() : new Date().getMinutes(),
+                                seconds: new Date().getSeconds(),
+                                month: this.months[new Date().getMonth()],
+                                year: new Date().getFullYear(),
+                            }
+                            
+                            subs.validity = this.validitySetter(newDate);
+                            this.amount = this.amount - subs.subamount;
+                            localStorage.setItem("amount", this.amount);
+
+                            localStorage.setItem("subscriptions", JSON.stringify(this.subscriptions));
+                            this.loadSubscription();
+                            this.history = this.history ? [...this.history, subs] : [subs];
+                            localStorage.setItem("history", JSON.stringify(this.history));
+                            this.loadHistory();
+                        }
+
+                    }
+
+                })
+            }
+
+
+            yield timeout(5000);
+        }
     }
 }
